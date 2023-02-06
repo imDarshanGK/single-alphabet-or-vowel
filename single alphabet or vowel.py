3b175155e5c86f1c29cd6bb1121737bb09f42a3a
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-02-06 22:30:29 +0530

    single alphabet

---
+++ single alphabet or vowel.py
@@ -0,0 +1,7 @@
+c=input("Enter single alphabet:")
+if c=='a' or c=='e' or c=='i' or c=='o' or c=='u':
+    print("Lower case vowel")
+elif c=='A' or c=='E' or c=='I' or c=='O' or c=='U':
+    print("Upper case vowel")
+else:
+    print("Consonant")
